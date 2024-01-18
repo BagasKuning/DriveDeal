ce48d4eb747ff8d1394dbc37400793f9e8a61232
BagasKuning <dwibagaskara55@gmail.com>
2024-01-18 14:29:25 +0700

    add public image

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+"use client"
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+const Hero = () => {
+    const handleScroll = () => {
+        
+    }
+
+  return (
+    <div className='hero'>
+        
+    </div>
+  )
+}
+
+export default Hero
